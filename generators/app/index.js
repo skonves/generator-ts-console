03494f299dfb880ae29ae0ae5edfce5782479372
Steve Konves <stephen.konves@gmail.com>
2018-05-17 07:51:03 -0700

    Add basic console app generator

---
+++ generators/app/index.js
@@ -0,0 +1,61 @@
+"use strict";
+
+const Generator = require("yeoman-generator");
+
+module.exports = class extends Generator {
+  initializing() {
+    this.pkg = this.fs.readJSON(this.destinationPath("package.json"), {});
+
+    if (this.pkg && Object.keys(this.pkg).length) {
+      console.log("Found existing package.json:");
+      console.log(this.pkg);
+    }
+  }
+
+  writing() {
+    [
+      ".prettierrc",
+      "tsconfig.json",
+      "tslint.json",
+      ".gitignore",
+      ".nycrc",
+      "src/index.ts",
+      "src/index.tests.ts"
+    ].forEach(file => {
+      this.fs.copy(this.templatePath(file), this.destinationPath(file));
+    });
+
+    this.fs.copyTpl(
+      this.templatePath("package.json"),
+      this.destinationPath("package.json"),
+      {
+        name: this.pkg.name || "typescript-application",
+        version: this.pkg.version || "0.0.1",
+        description: this.pkg.description || "",
+        author: this.pkg.author || "",
+        license: this.pkg.license || "ISC"
+      }
+    );
+  }
+
+  install() {
+    this.npmInstall(
+      [
+        "typescript",
+        "@types/node",
+        "prettier",
+        "tslint",
+        "mocha",
+        "@types/mocha",
+        "chai",
+        "@types/chai",
+        "nyc",
+        "ts-node",
+        "source-map-support"
+      ],
+      {
+        "save-dev": true
+      }
+    );
+  }
+};
