52ce0d80c5067653ca16c8d276ee0d3824e64882
Steve Konves <stephen.konves@gmail.com>
2020-10-25 16:46:57 -0700

    Generate README

--- src/npm/index.ts
+++ src/npm/index.ts
@@ -4,16 +4,39 @@ import * as Generator from 'yeoman-generator';
 import { exec } from '../utils';
 
 module.exports = class extends Generator {
+  name: string;
+  description: string;
+
   async configuring() {
-    if (!this.fs.exists(this.destinationPath('package.json'))) {
+    let json: any;
+    if (this.fs.exists(this.destinationPath('package.json'))) {
+      json = this.fs.readJSON(this.destinationPath('package.json'));
+    } else {
       await exec('npm init -y');
-      const json = await fs.readFile(this.destinationPath('package.json'));
+      json = JSON.parse(
+        (await fs.readFile(this.destinationPath('package.json'))).toString(),
+      );
       await fs.unlink(this.destinationPath('package.json'));
 
-      this.fs.writeJSON(
-        this.destinationPath('package.json'),
-        JSON.parse(json.toString()),
-      );
+      json.version = '0.0.1';
+      json.description =
+        'Base project for creating a console application in Typescript';
+
+      this.fs.writeJSON(this.destinationPath('package.json'), json);
     }
+
+    this.name = json.name;
+    this.description = json.description;
+  }
+
+  writing() {
+    this.fs.copyTpl(
+      this.templatePath('README.md.template'),
+      this.destinationPath('README.md'),
+      {
+        name: this.name,
+        description: this.description,
+      },
+    );
   }
 };
