f6fbb65013451bdc12f85d9067c8c195a0f446e5
Steve Konves <stephen.konves@gmail.com>
2020-10-27 21:20:30 -0700

    feat: support supplied options

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,3 @@
-import * as os from 'os';
-
 import * as Generator from 'yeoman-generator';
 
 module.exports = class extends Generator {
@@ -9,53 +7,54 @@ module.exports = class extends Generator {
   }
 
   async prompting() {
-    const answers = await this.prompt([
-      {
-        type: 'list',
-        name: 'mode',
-        message: 'Quick start',
-        choices: [
-          { name: 'Set it up for me', value: 'basic' },
-          { name: 'Advanced options', value: 'advanced' },
-        ],
-        default: 0,
-      },
-    ]);
+    const mode =
+      this.options.mode ||
+      (await this.prompt([
+        {
+          type: 'list',
+          name: 'mode',
+          message: 'Quick start',
+          choices: [
+            { name: 'Set it up for me', value: 'basic' },
+            { name: 'Advanced options', value: 'advanced' },
+          ],
+          default: 0,
+        },
+      ])).mode;
 
-    const typescriptArgs = [];
-    const linterArgs = [];
-    const testingArgs = [];
-    const ciArgs = [];
-    let licenseOptions: any = {
-      defaultLicense: 'MIT',
-    };
+    const basic = mode === 'basic';
 
-    if (answers.mode === 'basic') {
-      typescriptArgs.push('latest');
-      linterArgs.push('eslint');
-      testingArgs.push('jest');
-      ciArgs.push('github');
+    const typescriptArg =
+      this.options.typescript || (basic ? 'latest' : undefined);
+    const linterArg = this.options.linter || (basic ? 'eslint' : undefined);
+    const testingArg = this.options.testing || (basic ? 'jest' : undefined);
+    const ciArg = this.options.ci || (basic ? 'github' : undefined);
 
-      licenseOptions = {
-        name: '',
-        email: '',
-        website: '',
-        license: 'UNLICENSED',
-      };
-    }
+    const licenseOptions =
+      this.options.license ||
+      (basic
+        ? {
+            name: '',
+            email: '',
+            website: '',
+            license: 'UNLICENSED',
+          }
+        : {
+            defaultLicense: 'MIT',
+          });
 
-    this._with('../typescript', typescriptArgs);
-    this._with('../linter', linterArgs);
+    this._with('../typescript', typescriptArg);
+    this._with('../linter', linterArg);
     this._with('../formatting');
-    this._with('../testing', testingArgs);
-    this._with('../ci', ciArgs);
+    this._with('../testing', testingArg);
+    this._with('../ci', ciArg);
     this.composeWith(require.resolve('generator-license'), licenseOptions);
   }
 
-  private _with(namespace: string, args?: string[]) {
+  private _with(namespace: string, arg?: string) {
     this.composeWith(
       require.resolve(namespace),
-      args && args.length ? { arguments: args } : {},
+      arg ? { arguments: [arg] } : {},
     );
   }
 };
