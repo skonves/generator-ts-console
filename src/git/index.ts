8cd7cbe06dac7d0130a37df7507ec596bb24f71a
Steve Konves <stephen.konves@gmail.com>
2020-10-31 09:33:02 -0700

    fix: correctly write to destination path

--- src/git/index.ts
+++ src/git/index.ts
@@ -3,6 +3,6 @@ import { getNodeGitIgnore } from './network';
 
 module.exports = class extends Generator {
   async configuring() {
-    this.fs.write('.gitignore', await getNodeGitIgnore());
+    this.fs.write(this.destinationPath('.gitignore'), await getNodeGitIgnore());
   }
 };
