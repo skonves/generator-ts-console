eec5a90c2b54c148a97f5883420d5f5ab185efad
Steve Konves <stephen.konves@gmail.com>
2021-05-30 10:01:13 -0700

    Improve lint and lint:fix scripts and .gitignore

--- src/git/index.ts
+++ src/git/index.ts
@@ -1,8 +1,11 @@
 import * as Generator from 'yeoman-generator';
+import { append } from '../utils';
 import { getNodeGitIgnore } from './network';
 
 module.exports = class extends Generator {
   async configuring() {
     this.fs.write(this.destinationPath('.gitignore'), await getNodeGitIgnore());
+
+    append(this.fs, this.destinationPath('.gitignore'), 'lib');
   }
 };
